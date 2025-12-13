3350082b0ad388b780f0cfdb9a723e1178483911
Iliana Vasslides <ilianamvass@gmail.com>
2025-12-13 12:20:32 -0500

    edits

--- app.py
+++ app.py
@@ -14,7 +14,6 @@ DYNAMODB_TABLE_NAME = os.environ.get('DYNAMODB_TABLE_NAME', '')
 
 @app.on_s3_event(bucket=S3_BUCKET, events=['s3:ObjectCreated:*'], suffix='.json')
 def s3_handler(event):
-<<<<<<< HEAD
     # log event bucket and key 
     app.log.debug(f"Received bucket event: {event.bucket}, key: {event.key}")
     # read the file form s3 and parse it (deserialize) 
@@ -32,20 +31,6 @@ def get_s3_object(bucket, key):
         return json.loads(response['Body'].read().decode('utf-8'))
     except Exception as e:
         app.log.error(f"Error: {e}")
-=======
-    app.log.debug(f"Received bucket event: {event.bucket}, key: {event.key}")
-    data = get_s3_object(event.bucket, event.key)
-    insert_data_into_dynamodb(data)
-    return data
-
-def get_s3_object(bucket, key):
-    s3 = boto3.client('s3')
-    try:
-        response = s3.get_object(Bucket=bucket, Key=key)
-        return json.loads(response['Body'].read().decode('utf-8'))
-    except Exception as e:
-        print(e)
->>>>>>> 7f029cd45208f7c437918a08a366ee553f4eaf6f
 
 def insert_data_into_dynamodb(data):
     dynamodb = boto3.resource('dynamodb')
@@ -69,26 +54,16 @@ def insert_data_into_dynamodb(data):
 
 @app.route('/access', methods=['GET'])
 def get_access():
-<<<<<<< HEAD
     # return all records from DDB
     # using put method to add record
-=======
->>>>>>> 7f029cd45208f7c437918a08a366ee553f4eaf6f
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table(DYNAMODB_TABLE_NAME)
     try:
         items = table.scan()['Items']
-<<<<<<< HEAD
         # sorts the items using the following key (access_time)
-=======
->>>>>>> 7f029cd45208f7c437918a08a366ee553f4eaf6f
         sorted_items = sorted(items, key=lambda x: x['access_time'])
         return sorted_items
     except ClientError as e:
         app.log.error(f"Error scanning DynamoDB table: {e}")
-<<<<<<< HEAD
         raise e 
     
-=======
-        raise e
->>>>>>> 7f029cd45208f7c437918a08a366ee553f4eaf6f
